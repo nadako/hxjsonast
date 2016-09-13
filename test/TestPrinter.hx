21cbbd9784ab68a94294924f6056fae1450d22cb
Dan Korostelev <nadako@gmail.com>
2016-09-13 13:16:56 +0300

    add pretty-printer tests (closes #4)

--- test/TestPrinter.hx
+++ test/TestPrinter.hx
@@ -25,6 +25,22 @@ class TestPrinter {
         ])), '[true,null,{}]');
     }
 
+    public function test_print_pretty() {
+        check(mk(JObject([
+            f("hello", JNumber("16")),
+            f("world", JBool(false)),
+        ])), '{\n\t"hello": 16,\n\t"world": false\n}', "\t");
+        check(mk(JArray([
+            mk(JBool(true)),
+            mk(JNull),
+            mk(JObject([
+                f("what", JArray([
+                    mk(JString("hi"))
+                ]))
+            ]))
+        ])), '[\n\ttrue,\n\tnull,\n\t{\n\t\t"what": [\n\t\t\t"hi"\n\t\t]\n\t}\n]', "\t");
+    }
+
     static inline function check(json:Json, result:String, ?space:String) {
         equals(result, Printer.print(json, space));
     }
